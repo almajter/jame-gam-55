510a0a55135463cf32fa275a884377757c99eb41
dr6401 <dr6401@student.uni-lj.si>
2025-11-18 19:18:50 +0100

    Added soundManager

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    [SerializeField] private AudioSource sfxAudioSource;
+
+    [SerializeField] private List<AudioClip> audioClips;
+
+    private AudioClip audioClip;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +18,12 @@ public class SoundManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PlayClickSFX()
+    {
+        if (audioClips.Count == 0) return;
+        int index = Random.Range(0, audioClips.Count);
+        sfxAudioSource.PlayOneShot(audioClips[index]);
     }
 }
