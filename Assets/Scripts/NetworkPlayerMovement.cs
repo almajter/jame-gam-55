7ed4acf8796cca91c9d25da0863abf4624e2eb55
dr6401 <dr6401@student.uni-lj.si>
2025-11-18 16:33:29 +0100

    Multiplayer lobby added

--- Assets/Scripts/NetworkPlayerMovement.cs
+++ Assets/Scripts/NetworkPlayerMovement.cs
@@ -1,31 +1,23 @@
-using System;
+using Unity.Netcode;
 using UnityEngine;
 using Unity.Netcode.Components;
-public class ClientNetworkTransform : NetworkTransform
+
+public class NetworkPlayerMovement : NetworkBehaviour
 {
-    
     private float horizontal;
     private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
-    //[SerializeField] private Transform groundCheck;
-    //[SerializeField] private LayerMask groundLayer;
-
-    private void Start()
-    {
-        if (rb == null)
-        {
-            rb = GetComponent<Rigidbody2D>();   
-        }
-    }
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump"))//&& IsGrounded())
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
         }
@@ -43,10 +35,10 @@ public class ClientNetworkTransform : NetworkTransform
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
     }
 
-    /*private bool IsGrounded()
+    private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }*/
+    }
 
     private void Flip()
     {
@@ -57,9 +49,5 @@ public class ClientNetworkTransform : NetworkTransform
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
-    }
-    protected override bool OnIsServerAuthoritative()
-    {
-        return false;
     }
 }
