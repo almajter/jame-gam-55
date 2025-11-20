c9a662a84ea2df789a0a60f7ac5ed96f1e910a9d
dominikpin <dp4550@student.uni-lj.si>
2025-11-20 10:38:01 +0100

    added kick indicator

--- Assets/Scripts/NetworkPlayerMovement.cs
+++ Assets/Scripts/NetworkPlayerMovement.cs
@@ -6,36 +6,30 @@ using Unity.Netcode.Components;
 public class NetworkPlayerMovement : NetworkBehaviour
 {
     private float horizontal;
-    [SerializeField] private float speed = 8f;
-    [SerializeField] private float jumpingPower = 16f;
     private bool isFacingRight = true;
+    private bool isZPressed;
+    private bool isPowerGoingUp;
 
+    [SerializeField] private float speed = 6f;
+    [SerializeField] private float jumpingPower = 6f;
+    [SerializeField] private float kickPower = 2f;
     [SerializeField] private Rigidbody2D rb;
-    //[SerializeField] private Transform groundCheck;
-    //[SerializeField] private LayerMask groundLayer;
-
-    private void Start()
-    {
-        if (rb == null)
-        {
-            rb = GetComponent<Rigidbody2D>();
-        }
-    }
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private GameObject powerBarGameObject;
+    [SerializeField] private GameObject kickPowerIndicator;
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump"))// && IsGrounded())
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
         }
 
-        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
-        }
-
+        HandleKickPower(KeyCode.Z);
+        HandlePowerBar();
         Flip();
     }
 
@@ -44,10 +38,10 @@ public class NetworkPlayerMovement : NetworkBehaviour
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
     }
 
-    /*private bool IsGrounded()
+    private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }*/
+    }
 
     private void Flip()
     {
@@ -60,6 +54,88 @@ public class NetworkPlayerMovement : NetworkBehaviour
         }
     }
 
+    private void HandleKickPower(KeyCode key)
+    {
+        if (Input.GetKeyDown(key))
+        {
+            SetKickPower(true);
+        }
+        else if (Input.GetKeyUp(key))
+        {
+            SetKickPower(false);
+        }
+    }
+
+    private void SetKickPower(bool isPressed)
+    {
+        isZPressed = isPressed;
+        powerBarGameObject.SetActive(isPressed);
+
+        if (isPressed)
+        {
+            kickPowerIndicator.transform.localScale = new Vector3(0.3f, 0f, 0f);
+        }
+        else
+        {
+            HandleKick();
+        }
+    }
+
+    private void HandlePowerBar()
+    {
+        if (isZPressed)
+        {
+            float diff = Time.deltaTime * 0.5f * (isPowerGoingUp ? 1f : -1f);
+            float newY = kickPowerIndicator.transform.localScale.y + diff;
+            newY = Mathf.Clamp(newY, 0f, 1f);
+            kickPowerIndicator.transform.localScale = new Vector3(0.3f, newY, 0);
+
+            if (newY >= 1f)
+            {
+                isPowerGoingUp = false;
+            }
+            else if (newY <= 0f)
+            {
+                isPowerGoingUp = true;
+            }
+        }
+    }
+
+    private void HandleKick()
+    {
+        float checkDistance = 1f;
+        Vector3 position = transform.position;
+        position.x = position.x + (isFacingRight ? 1 : -1) * 0.6f;
+        RaycastHit2D hit2D = Physics2D.Raycast(position, isFacingRight ? Vector2.right : Vector2.left, checkDistance);
+
+        if (hit2D.collider != null && hit2D.collider.CompareTag("Player"))
+        {
+            ulong targetId = hit2D.collider.GetComponent<NetworkObject>().OwnerClientId;
+            RequestKickServerRpc(targetId);
+        }
+        // TODO add kick depending on kickPowerIndicator.transform.localScale.y (0-1)
+    }
+
+    [ServerRpc]
+    void RequestKickServerRpc(ulong targetClientId)
+    {
+        ApplyKickClientRpc(targetClientId, new ClientRpcParams
+        {
+            Send = new ClientRpcSendParams
+            {
+                TargetClientIds = new[] { targetClientId }
+            }
+        });
+    }
+
+    [ClientRpc]
+    void ApplyKickClientRpc(ulong targetClientId, ClientRpcParams rpcParams = default)
+    {
+        if (NetworkManager.Singleton.LocalClientId != targetClientId)
+            return;
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower * kickPower);
+    }
+
     void ApplyJumpPowerUp(float jumpMultiplier)
     {
         jumpingPower *= jumpMultiplier;
@@ -70,7 +146,7 @@ public class NetworkPlayerMovement : NetworkBehaviour
     {
         GameEvents.OnJumpBoostPickedUp += ApplyJumpPowerUp;
     }
-    
+
     private void OnDisable()
     {
         GameEvents.OnJumpBoostPickedUp -= ApplyJumpPowerUp;
--- Assets/Scripts/NetworkStuff/ClientNetworkTransform.cs
+++ Assets/Scripts/NetworkStuff/ClientNetworkTransform.cs
@@ -3,79 +3,8 @@ using UnityEngine;
 using Unity.Netcode.Components;
 public class ClientNetworkTransform : NetworkTransform
 {
-    /*
-    private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
-    private bool isFacingRight = true;
-
-    [SerializeField] private Rigidbody2D rb;
-    //[SerializeField] private Transform groundCheck;
-    //[SerializeField] private LayerMask groundLayer;
-
-    private void Start()
-    {
-        if (rb == null)
-        {
-            rb = GetComponent<Rigidbody2D>();   
-        }
-    }
-
-    void Update()
-    {
-        horizontal = Input.GetAxisRaw("Horizontal");
-
-        if (Input.GetButtonDown("Jump"))//&& IsGrounded())
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
-        }
-
-        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
-        }
-
-        Flip();
-    }
-
-    private void FixedUpdate()
-    {
-        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
-    }
-
-    /*private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
-
-    private void Flip()
-    {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
-    }*/
     protected override bool OnIsServerAuthoritative()
     {
         return false;
     }
-    /*
-    void ApplyJumpPowerUp(float jumpMultiplier)
-    {
-        jumpingPower *= jumpMultiplier;
-        Debug.Log($"jump increased by {(jumpMultiplier - 1) * 100}%");
-    }
-
-    private void OnEnable()
-    {
-        GameEvents.OnJumpBoostPickedUp += ApplyJumpPowerUp;
-    }
-    
-    private void OnDisable()
-    {
-        GameEvents.OnJumpBoostPickedUp -= ApplyJumpPowerUp;
-    }*/
 }
